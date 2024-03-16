c51353c502fceb2fd6a86f5bd6a2afeb22ac6ece
karshfish <87549809+karshfish@users.noreply.github.com>
2024-03-16 13:48:01 +0300

    just adding comments

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -7,12 +7,13 @@ namespace Framework;
 class TemplateEngine
 {
 
-    public function __construct(private string $basePath)
+    public function __construct(private string $basePath) //this property is for identifying the template tha we are gonna use
     {
     }
-    public function render(string $template, array $data = [])
+    public function render(string $template, array $data = []) /*template variable identifies what page the controller will use for the url path.
+                                                                the array data is to pass data into the template page  */
     {
-        extract($data, EXTR_SKIP);
-        include "{$this->basePath}/{$template}";
+        extract($data, EXTR_SKIP);  //Extract array data into variable that we can use separately
+        include "{$this->basePath}/{$template}"; // including the full path of the template page
     }
 }
