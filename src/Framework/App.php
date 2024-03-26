26f95af875b9e985623f2c497f962ad973566401
karshfish <87549809+karshfish@users.noreply.github.com>
2024-03-26 11:08:16 -0700

    Update App.php
    comments

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -10,7 +10,7 @@ class App
     private Router $router;
     private Container $container;
 
-    public function __construct(string $containerDefinitionsPath = NULL)
+    public function __construct(string $containerDefinitionsPath = NULL) //Making the first thing to do when creating the App class is to search for the container definiions while making it optional 
     {
         $this->router = new Router();
         $this->container = new Container();
@@ -19,17 +19,17 @@ class App
             $this->container->addDefinitions($containerDefinitions);
         }
     }
-    public function run()
+    public function run() //To run the rout if it exist
     {
         $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $method = $_SERVER['REQUEST_METHOD'];
         $this->router->dispatch($path, $method, $this->container);
     }
-    public function get(string $path, array $controller)
+    public function get(string $path, array $controller) //Adding a route to the routes list using get method
     {
         $this->router->add('GET', $path, $controller);
     }
-    public function addMiddleware($middleware)
+    public function addMiddleware($middleware) //Adding a Middleware to the list 
     {
         $this->router->addMiddleware($middleware);
     }
