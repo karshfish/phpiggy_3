d8179a2082633cb88d1c342c3bd57cbde3e7422c
karshfish <87549809+karshfish@users.noreply.github.com>
2024-04-08 04:30:26 -0700

    Update Validator.php

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -15,14 +15,22 @@ class Validator
     }
     public function validate(array $formData, array $fields)
     {
+        $errors = [];
         foreach ($fields as $fieldName => $rules) {
             foreach ($rules as $rule) {
                 $ruleValidator = $this->rules[$rule];
                 if ($ruleValidator->validate($formData, $fieldName, [])) {
                     continue;
                 }
-                echo 'Error';
+                $errors[$fieldName][] = $ruleValidator->getMessage(
+                    $formData,
+                    $fieldName,
+                    []
+                );
             }
+        }
+        if (count($errors)) {
+            dd($errors);
         }
     }
 }
