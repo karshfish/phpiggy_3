005e3dd42a84843df3ddd8a220df3c40a2c75450
karshfish <87549809+karshfish@users.noreply.github.com>
2024-04-21 05:20:10 -0700

    Update SessionMiddleware.php

--- src/App/Middleware/SessionMiddleware.php
+++ src/App/Middleware/SessionMiddleware.php
@@ -5,11 +5,18 @@ declare(strict_types=1);
 namespace App\Middleware;
 
 use Framework\Contracts\MiddlewareInterface;
+use App\Exceptions\SessionExceptions;
 
 class SessionMiddleware implements MiddlewareInterface
 {
     public function process(callable $next)
     {
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            throw new SessionExceptions("Session already active.");
+        }
+        if (headers_sent()) {
+            throw new SessionExceptions("Headersalready sent");
+        }
         session_start();
         $next();
     }
