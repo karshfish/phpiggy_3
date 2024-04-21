7c110d56ff7a859d2f9515a6208fab01d095b4f9
karshfish <87549809+karshfish@users.noreply.github.com>
2024-04-21 04:33:07 -0700

    catching and printing errors

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -31,7 +31,7 @@ class Validator
             }
         }
         if (count($errors)) {
-            throw new ValidationException();
+            throw new ValidationException($errors);
         }
     }
 }
