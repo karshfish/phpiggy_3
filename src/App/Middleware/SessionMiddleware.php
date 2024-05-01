f6969c9d6b9997b173664a06a054215f02f4370a
karshfish <87549809+karshfish@users.noreply.github.com>
2024-05-01 03:21:01 -0700

    Update SessionMiddleware.php

--- src/App/Middleware/SessionMiddleware.php
+++ src/App/Middleware/SessionMiddleware.php
@@ -14,8 +14,8 @@ class SessionMiddleware implements MiddlewareInterface
         if (session_status() === PHP_SESSION_ACTIVE) {
             throw new SessionExceptions("Session already active.");
         }
-        if (headers_sent()) {
-            throw new SessionExceptions("Headersalready sent");
+        if (headers_sent($filename, $line)) {
+            throw new SessionExceptions("Headers already sent. Consider Enabling Output buffering data from {$filename} - Line {$line}");
         }
         session_start();
         $next();
