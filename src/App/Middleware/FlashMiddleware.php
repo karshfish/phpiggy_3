b80e0ff578df934e192feb7e466bfddf9550f5b7
karshfish <87549809+karshfish@users.noreply.github.com>
2024-05-13 12:04:44 +0300

    Update FlashMiddleware.php

--- src/App/Middleware/FlashMiddleware.php
+++ src/App/Middleware/FlashMiddleware.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 namespace App\Middleware;
 
 use Framework\Contracts\MiddlewareInterface;
+use Framework\TemplateEngine as TE;
 
 class  FlashMiddleware implements MiddlewareInterface
 {
+    public function __construct(private TE $view)
+    {
+    }
     public function process(callable $next)
     {
+        $this->view->addGlobal('errors', $_SESSION['errors'] ?? []);
         $next();
     }
 }
