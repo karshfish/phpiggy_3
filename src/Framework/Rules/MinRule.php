ae72861a25a6a0e74795786cbc63d6e4e4a2be69
karshfish <87549809+karshfish@users.noreply.github.com>
2024-06-29 14:13:52 +0300

    Update MinRule.php

--- src/Framework/Rules/MinRule.php
+++ src/Framework/Rules/MinRule.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 namespace Framework\Rules;
 
 use Framework\Contracts\RuleInterface;
+use InvalidArgumentException as IAE;
 
 class MinRule implements RuleInterface
 {
     public function validate(array $data, string $field, array $parms): bool
     {
-        if ($data[$field] < $parms[0]) {
-            return false;
-        } else {
-            return true;
+        if (empty($parms[0])) {
+            throw new IAE("Minimum length not specified");
         }
+        $length = $parms[0];
+        return $data[$field] >= $length;
     }
     public function getMessage(array $data, string $field, array $params): string
     {
-        return "You are under required age.";
+        return "You are under required age. Must be at least {$params[0]}";
     }
 }
