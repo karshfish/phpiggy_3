f36813eb4df92d5ec467bfad2548130071ea9a2b
karshfish <87549809+karshfish@users.noreply.github.com>
2024-11-20 15:20:11 +0300

    refactoring database

--- cli.php
+++ cli.php
@@ -1,9 +1,20 @@
 <?php
-$driver = 'mysql';
-$config = http_build_query($data = [
-    'host' => 'localhost',
-    'port' => 3306,
-    'name' => 'phpiggy'
-], arg_separator: ";");
-$dsn = "{$driver}:{$config}";
-echo $dsn;
+include __DIR__ . "/src/Framework/Database.php";
+
+use Framework\Database;
+
+$db = new Database(
+    driver: "mysql",
+    config: [
+        "host" => 'localhost',
+        'port' => 3306,
+        'dbname' => 'phpiggy'
+    ],
+    username: 'root',
+    password: ''
+);
+echo "Connection with database established \n ";
+
+$query = "SELECT * FROM products";
+$stmt = $db->connection->query($query);
+var_dump($stmt->fetchAll());
