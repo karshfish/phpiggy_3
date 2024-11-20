0676fe93a26a31a2203375a9ad975b6615d2f08b
karshfish <87549809+karshfish@users.noreply.github.com>
2024-11-21 00:11:54 +0300

    from prepare functions to transactions

--- cli.php
+++ cli.php
@@ -15,6 +15,26 @@ $db = new Database(
 );
 echo "Connection with database established \n ";
 
-$query = "SELECT * FROM products";
-$stmt = $db->connection->query($query);
-var_dump($stmt->fetchAll());
+try {
+    $db->connection->beginTransaction();   //Begin a transaction
+    $db->connection->query("INSERT INTO products VALUES(100, 'Gloves')");  //Dummy query to simulate a transaction
+
+    $cond = "Gloves";
+    $query = "SELECT * FROM products WHERE name = :name";
+
+    $stmt = $db->connection->prepare($query); // This to make a prepared statement to prevent SQl injection
+
+    $stmt->bindValue('name', $cond, PDO::PARAM_STR); //To bind values in the prepared statement without executing the script
+
+    $stmt->execute(); // To execute our statement keep in mind this important so that we can fetch a prepared statement
+
+    var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
+
+    $db->connection->commit(); // End transaction
+
+} catch (Exception $error) {
+    if ($db->connection->inTransaction()) { //Checking If we are in a transaction or not
+        $db->connection->rollBack(); // Revert Changes happens in the transaction if it's failed 
+        echo "Transaction failed";
+    }
+}
