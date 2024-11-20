33a4d0bb8e1f78c83a06be6b48bf143b24d5dce0
karshfish <87549809+karshfish@users.noreply.github.com>
2024-11-21 00:51:38 +0300

    creating the database and trying to modify it (failed attempt to modify)

--- cli.php
+++ cli.php
@@ -15,26 +15,30 @@ $db = new Database(
 );
 echo "Connection with database established \n ";
 
-try {
-    $db->connection->beginTransaction();   //Begin a transaction
-    $db->connection->query("INSERT INTO products VALUES(100, 'Gloves')");  //Dummy query to simulate a transaction
+// try {
+//     $db->connection->beginTransaction();   //Begin a transaction
+//     $db->connection->query("INSERT INTO products VALUES(100, 'Gloves')");  //Dummy query to simulate a transaction
 
-    $cond = "Gloves";
-    $query = "SELECT * FROM products WHERE name = :name";
+//     $cond = "Gloves";
+//     $query = "SELECT * FROM products WHERE name = :name";
 
-    $stmt = $db->connection->prepare($query); // This to make a prepared statement to prevent SQl injection
+//     $stmt = $db->connection->prepare($query); // This to make a prepared statement to prevent SQl injection
 
-    $stmt->bindValue('name', $cond, PDO::PARAM_STR); //To bind values in the prepared statement without executing the script
+//     $stmt->bindValue('name', $cond, PDO::PARAM_STR); //To bind values in the prepared statement without executing the script
 
-    $stmt->execute(); // To execute our statement keep in mind this important so that we can fetch a prepared statement
+//     $stmt->execute(); // To execute our statement keep in mind this important so that we can fetch a prepared statement
 
-    var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
+//     var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
 
-    $db->connection->commit(); // End transaction
+//     $db->connection->commit(); // End transaction
 
-} catch (Exception $error) {
-    if ($db->connection->inTransaction()) { //Checking If we are in a transaction or not
-        $db->connection->rollBack(); // Revert Changes happens in the transaction if it's failed 
-        echo "Transaction failed";
-    }
-}
+// } catch (Exception $error) {
+//     if ($db->connection->inTransaction()) { //Checking If we are in a transaction or not
+//         $db->connection->rollBack(); // Revert Changes happens in the transaction if it's failed 
+//         echo "Transaction failed";
+//     }
+// }
+$sqlFile = file_get_contents("./database.sql");
+$db->connection->query($sqlFile);
+// $db->connection->query("ALTER TABLE users MODIFY COLUMN age unsigned;");
+// echo "Modifying done";
