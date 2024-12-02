c87324134160ff5dbec25e3be2df1e770befc7f0
karshfish <87549809+karshfish@users.noreply.github.com>
2024-12-02 14:28:37 +0200

    adding DB injection

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 use Framework\TemplateEngine as TE;
 use App\Config\Paths as PATH;
 use App\Services\ValidatorService as VS;
+use Framework\Database as DB;
 
 return [
     TE::class => fn () => new TE(PATH::VIEW),
-    VS::class => fn () => new VS
+    VS::class => fn () => new VS,
+    DB::class => fn () => new DB()
 ];
