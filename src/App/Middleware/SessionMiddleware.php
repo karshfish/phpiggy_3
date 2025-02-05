bcb51b35903c64d62393148fccef250c7c6196ed
karshfish <87549809+karshfish@users.noreply.github.com>
2025-02-05 18:22:43 +0200

    securing cookies

--- src/App/Middleware/SessionMiddleware.php
+++ src/App/Middleware/SessionMiddleware.php
@@ -17,6 +17,11 @@ class SessionMiddleware implements MiddlewareInterface
         if (headers_sent($filename, $line)) { //To make sure that a session can't be restarted without all data has been sent first
             throw new SessionExceptions("Headers already sent. Consider Enabling Output buffering data from {$filename} - Line {$line}");
         }
+        session_set_cookie_params([
+            'secure' => $_ENV['APP_ENV'] === 'production',
+            'httponly' => true,
+            'samesite' => 'lax'
+        ]);
         session_start();
         $next();
         session_write_close();
