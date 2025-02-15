2f81ff66b9e06558a7f49f0c4fec93b5daaffe7a
karshfish <87549809+karshfish@users.noreply.github.com>
2025-02-15 10:33:51 +0200

    Create NumericRule.php

---
+++ src/Framework/Rules/NumericRule.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Framework\Rules;
+
+use InvalidArgumentException as IAE;
+
+use Framework\Contracts\RuleInterface;
+
+class NumericRule implements RuleInterface
+{
+    public function validate(array $data, string $field, array $params): bool
+    {
+        return is_numeric($data[$field]);
+    }
+    public function getMessage(array $data, string $field, array $params): string
+    {
+        return "Invalid amount";
+    }
+}
