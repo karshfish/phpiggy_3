0a86cf5d808c785ce9a91c94ff0dee0ddfcc314c
karshfish <87549809+karshfish@users.noreply.github.com>
2025-02-15 11:29:20 +0200

    Update TransactionsController.php

--- src/App/Controllers/TransactionsController.php
+++ src/App/Controllers/TransactionsController.php
@@ -5,13 +5,15 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Framework\TemplateEngine;
-use App\Services\ValidatorService as VS;
+use App\Services\{ValidatorService as VS, TransactionService as TS};
+
 
 class TransactionsController
 {
     public function __construct(
         private TemplateEngine $view,
-        private VS $validatorService
+        private VS $validatorService,
+        private TS $ts
     ) {}
 
     public function createView()
@@ -21,6 +23,7 @@ class TransactionsController
     public function createTransaction()
     {
         $this->validatorService->validateCreate($_POST);
+        $this->ts->create($_POST);
     }
     public function deleteTransaction() {}
 }
