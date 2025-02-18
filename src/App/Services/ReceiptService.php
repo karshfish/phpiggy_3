e05ac87eb629925180fdd03effb7da8ed02d9eb5
karshfish <87549809+karshfish@users.noreply.github.com>
2025-02-18 16:20:00 +0200

    Validating the receipt file

--- src/App/Services/ReceiptService.php
+++ src/App/Services/ReceiptService.php
@@ -14,6 +14,22 @@ class ReceiptService
     ) {}
     public function validateFile(?array $file)
     {
+        if (!$file || $file['error'] !== UPLOAD_ERR_OK) {
+            throw new VE(['receipt' => ['failed to upload the file']]);
+        }
+        $maxFileSize = 3 * 1024 * 1024;
+        if ($file['size'] > $maxFileSize) {
+            throw new VE(['receipt' => ['File is too large']]);
+        }
+        $originalFileName = $file['name'];
+        if (!preg_match('/^[A-za-z0-9\s._-]+$/', $originalFileName)) {
+            throw new VE(['receipt' => ['Wrong file name']]);
+        }
+        $fileMimeType = $file['type'];
+        $allowedMimeTypes = ['image/png', 'image/jpeg', 'application/pdf'];
+        if (!in_array($fileMimeType, $allowedMimeTypes)) {
+            throw new VE(['receipt' => ['Wrong file type']]);
+        }
         dd($file);
     }
 }
