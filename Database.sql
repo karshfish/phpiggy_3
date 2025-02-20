862697dbc5d3ca9a0f852f2c55a5616ad9a5b5b6
karshfish <87549809+karshfish@users.noreply.github.com>
2025-02-20 23:29:28 +0200

    receipts tab;e

--- Database.sql
+++ Database.sql
@@ -21,3 +21,14 @@ CREATE TABLE IF NOT EXISTS users  (
     PRIMARY KEY (id),
     FOREIGN KEY(user_id) REFERENCES users(id)
  );
+ CREATE TABLE IF NOT EXISTS  transaction_receipts(
+    id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    originalFilename varchar(255)  NOT NULL,
+    storageFilename varchar(255) NOT NULL,
+    mediaType varchar(255) NOT NULL,
+    transaction_id bigint(20) UNSIGNED NOT NULL,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),  
+    updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    PRIMARY KEY (id),
+    FOREIGN KEY(transaction_id) REFERENCES transactions(id)
+ );
